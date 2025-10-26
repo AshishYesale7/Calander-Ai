34b88557f4d5864dd19d729c77a720a5e8904a91
Ashish Yesale <ashishyesale007@gmail.com>
2025-10-26 17:38:10 +0000

    i think you removed the bigger  flame and make the streakshowcase width

--- src/components/landing/FeatureShowcase.tsx
+++ src/components/landing/FeatureShowcase.tsx
@@ -58,6 +58,28 @@ const DailyFlameIcon = ({ isComplete, className }: { isComplete: boolean, classN
     </svg>
 );
 
+const TrophyFlameIcon = ({ isComplete, className }: { isComplete: boolean, className?: string }) => (
+    <svg height="200px" width="200px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xmlSpace="preserve" className={className}>
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+        <g id="SVGRepo_iconCarrier">
+            <g style={{ filter: isComplete ? 'none' : 'grayscale(100%)' }}>
+                <path style={{fill:'#971D2E'}} d="M34.595,462.184c0,27.51,22.307,49.816,49.816,49.816h343.178v-33.211L34.595,462.184z"></path>
+                <path style={{fill:'#BE2428'}} d="M427.589,412.368v66.422H84.411c-27.51,0-49.816-7.439-49.816-16.605 c0-27.51,22.307-49.816,49.816-49.816H427.589z"></path>
+                <path style={{fill:'#FFD46E'}} d="M427.589,412.368V512c27.51,0,49.816-22.307,49.816-49.816 C477.405,434.674,455.099,412.368,427.589,412.368z"></path>
+                <path style={{fill:'#FFE9B7'}} d="M427.589,412.368c9.166,0,16.605,22.307,16.605,49.816c0,27.51-7.439,49.816-16.605,49.816 c-27.51,0-49.816-22.307-49.816-49.816C377.773,434.674,400.08,412.368,427.589,412.368z"></path>
+                <path style={{fill:'#EC5123'}} d="M401.939,205.099L353.562,60.355l-18,11.037l-77.99,340.964 c84.876-0.841,153.412-69.898,153.412-154.973C410.984,239.04,407.796,221.439,401.939,205.099z"></path>
+                <path style={{fill:'#F27524'}} d="M370.666,205.099c4.594,16.34,7.107,33.941,7.107,52.285c0,84.931-53.669,153.899-120.201,154.973 c-0.52,0.011-1.052,0.011-1.572,0.011c-85.595,0-154.984-69.388-154.984-154.984c0-18.343,3.188-35.945,9.044-52.285l48.377-144.744 l37.772,23.17L256,8.303l59.791,75.222l19.771-12.133L370.666,205.099z"></path>
+                <path style={{fill:'#FFD46E'}} d="M325.488,202.475L256,115.053v230.893c48.831,0,88.562-39.731,88.562-88.562 C344.562,237.192,337.953,218.195,325.488,202.475z"></path>
+                <path style={{fill:'#FFE9B7'}} d="M299.429,202.475c7.793,15.72,11.923,34.716,11.923,54.909c0,48.831-24.831,88.562-55.351,88.562 c-48.831,0-88.562-39.731-88.562-88.562c0-20.192,6.609-39.189,19.118-54.953L256,115.053L299.429,202.475z"></path>
+                <g> <circle style={{fill:'#EC5123'}} cx="444.195" cy="257.384" r="8.303"></circle> <circle style={{fill:'#EC5123'}} cx="315.791" cy="131.371" r="8.303"></circle> </g>
+                <g> <circle style={{fill:'#F27524'}} cx="158.438" cy="8.303" r="8.303"></circle> <circle style={{fill:'#F27524'}} cx="67.805" cy="257.384" r="8.303"></circle> <circle style={{fill:'#F27524'}} cx="67.805" cy="224.173" r="8.303"></circle> </g>
+                <g> <circle style={{fill:'#FFD46E'}} cx="256" cy="224.173" r="8.303"></circle> <circle style={{fill:'#FFD46E'}} cx="289.211" cy="257.384" r="8.303"></circle> </g>
+            </g>
+        </g>
+    </svg>
+);
+
 
 const StreakShowcase = () => {
     const weekDays = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
@@ -65,6 +87,7 @@ const StreakShowcase = () => {
 
     return (
         <div className="relative h-64 w-full flex items-center justify-center">
+            <TrophyFlameIcon isComplete={true} className="absolute inset-0 h-full w-full opacity-10" />
             <div className="relative flex flex-col items-center gap-4">
                 <h4 className="text-xl font-bold text-white">Build Your Streak</h4>
                 <div className="flex justify-around gap-2 sm:gap-4 p-4 bg-black/25 rounded-2xl border border-white/10">
