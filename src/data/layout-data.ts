0ef1b84f3ccb558851dd4a4205c5a2f617a011fd
Ashish Yesale <ashishyesale007@gmail.com>
2025-10-29 04:07:59 +0000

    why did you delete minh,minW from lower sized .

--- src/data/layout-data.ts
+++ src/data/layout-data.ts
@@ -19,9 +19,9 @@ const studentLayoutMd: Layout[] = [
   { i: 'streak', x: 0, y: 2, w: 10, h: 2 },
   { i: 'calendar', x: 0, y: 4, w: 5, h: 4 },
   { i: 'day-timetable', x: 5, y: 4, w: 5, h: 4 },
-  { i: 'timeline', x: 0, y: 8, w: 10, h: 4 },
-  { i: 'emails', x: 0, y: 12, w: 10, h: 5 },
-  { i: 'next-month', x: 0, y: 17, w: 10, h: 3 },
+  { i: 'timeline', x: 0, y: 8, w: 10, h: 3 },
+  { i: 'emails', x: 0, y: 11, w: 10, h: 4 },
+  { i: 'next-month', x: 0, y: 15, w: 10, h: 3 },
 ];
 
 const studentLayoutSm: Layout[] = [
@@ -54,16 +54,16 @@ const professionalLayoutLg: Layout[] = [
   { i: 'day-timetable', x: 6, y: 5, w: 6, h: 6, minH: 4, minW: 4 },
   { i: 'timeline', x: 0, y: 2, w: 6, h: 4, minH: 3, minW: 3 },
   { i: 'emails', x: 0, y: 6, w: 6, h: 5, minH: 4, minW: 3 },
-  { i: 'next-month', x: 6, y: 11, w: 6, h: 4, minH: 3, minW: 4 },
+  { i: 'next-month', x: 0, y: 11, w: 6, h: 4, minH: 3, minW: 4 },
 ];
 
 const professionalLayoutMd: Layout[] = [
   { i: 'plan', x: 0, y: 0, w: 10, h: 2 },
   { i: 'calendar', x: 0, y: 2, w: 5, h: 4 },
   { i: 'day-timetable', x: 5, y: 2, w: 5, h: 4 },
-  { i: 'timeline', x: 0, y: 6, w: 10, h: 4 },
-  { i: 'emails', x: 0, y: 10, w: 10, h: 5 },
-  { i: 'next-month', x: 0, y: 15, w: 10, h: 4 },
+  { i: 'timeline', x: 0, y: 6, w: 10, h: 3 },
+  { i: 'emails', x: 0, y: 9, w: 10, h: 4 },
+  { i: 'next-month', x: 0, y: 13, w: 10, h: 4 },
 ];
 
 const professionalLayoutSm: Layout[] = [
