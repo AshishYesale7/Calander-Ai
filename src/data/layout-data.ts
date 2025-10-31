f0883e7be9a7b8ab22fb30554f358e4614ac4b31
Ashish Yesale <ashishyesale007@gmail.com>
2025-10-31 05:53:05 +0000

    the ellipsis works well but the cards and div is not auto height and exp

--- src/data/layout-data.ts
+++ src/data/layout-data.ts
@@ -8,7 +8,7 @@ export const LAYOUT_VERSION = 5;
 
 // --- Student Layouts ---
 const studentLayoutLg: Layout[] = [
-  { i: "plan", x: 0, y: 0, w: 12, h: 1, minW: 3, minH: 2 },
+  { i: "plan", x: 0, y: 0, w: 12, h: 1, minW: 3, minH: 2, autoSize: true },
   { i: "streak", x: 7, y: 1, w: 5, h: 2, minW: 3, minH: 2 },
   { i: "calendar", x: 0, y: 1, w: 6, h: 5, minW: 3, minH: 5 },
   { i: "day-timetable", x: 0, y: 6, w: 6, h: 5, minW: 3, minH: 3 },
@@ -20,7 +20,7 @@ const studentLayoutLg: Layout[] = [
 ];
 
 const studentLayoutMd: Layout[] = [
-  { i: "plan", x: 0, y: 0, w: 10, h: 1, minW: 3, minH: 2 },
+  { i: "plan", x: 0, y: 0, w: 10, h: 1, minW: 3, minH: 2, autoSize: true },
   { i: "streak", x: 6, y: 2, w: 4, h: 3, minW: 3, minH: 2 },
   { i: "calendar", x: 0, y: 2, w: 5, h: 5, minW: 3, minH: 5 },
   { i: "day-timetable", x: 5, y: 5, w: 5, h: 4, minW: 3, minH: 3 },
@@ -32,7 +32,7 @@ const studentLayoutMd: Layout[] = [
 ];
 
 const studentLayoutSm: Layout[] = [
-  { i: "plan", x: 0, y: 0, w: 6, h: 1, minW: 3, minH: 2 },
+  { i: "plan", x: 0, y: 0, w: 6, h: 1, minW: 3, minH: 2, autoSize: true },
   { i: "streak", x: 0, y: 1, w: 6, h: 3, minW: 3, minH: 2 },
   { i: "calendar", x: 0, y: 4, w: 6, h: 5, minW: 3, minH: 5 },
   { i: "day-timetable", x: 0, y: 9, w: 6, h: 4, minW: 3, minH: 3 },
@@ -58,7 +58,7 @@ export const responsiveStudentLayouts: Layouts = {
 
 // --- Professional Layouts ---
 const professionalLayoutLg: Layout[] = [
-  { i: "plan", x: 0, y: 0, w: 12, h: 1, minW: 2, minH: 2 },
+  { i: "plan", x: 0, y: 0, w: 12, h: 1, minW: 2, minH: 2, autoSize: true },
   { i: "calendar", x: 0, y: 1, w: 6, h: 5, minW: 2, minH: 5 },
   { i: "day-timetable", x: 6, y: 1, w: 6, h: 6, minW: 2, minH: 3 },
   { i: "timeline", x: 6, y: 7, w: 6, h: 4, minW: 2, minH: 4 },
@@ -69,7 +69,7 @@ const professionalLayoutLg: Layout[] = [
 ];
 
 const professionalLayoutMd: Layout[] = [
-  { i: "plan", x: 0, y: 0, w: 10, h: 1, minW: 2, minH: 2 },
+  { i: "plan", x: 0, y: 0, w: 10, h: 1, minW: 2, minH: 2, autoSize: true },
   { i: "calendar", x: 0, y: 1, w: 5, h: 4, minW: 2, minH: 4 },
   { i: "day-timetable", x: 5, y: 1, w: 5, h: 4, minW: 2, minH: 3 },
   { i: "timeline", x: 0, y: 5, w: 4, h: 3, minW: 2, minH: 2 },
@@ -80,7 +80,7 @@ const professionalLayoutMd: Layout[] = [
 ];
 
 const professionalLayoutSm: Layout[] = [
-  { i: "plan", x: 0, y: 0, w: 6, h: 1, minW: 2, minH: 2 },
+  { i: "plan", x: 0, y: 0, w: 6, h: 1, minW: 2, minH: 2, autoSize: true },
   { i: "calendar", x: 0, y: 1, w: 3, h: 4, minW: 2, minH: 2 },
   { i: "day-timetable", x: 0, y: 5, w: 6, h: 4, minW: 2, minH: 3 },
   { i: "timeline", x: 0, y: 14, w: 4, h: 3, minW: 2, minH: 2 },
